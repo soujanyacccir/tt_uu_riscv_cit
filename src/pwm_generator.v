f7c0e8cdfff0cffe17a0c846cd471e8e79f0856b
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-18 10:38:23 +0530

    Create pwm_generator.v

---
+++ src/pwm_generator.v
@@ -0,0 +1,21 @@
+module pwm_generator(
+    input  wire       clk,
+    input  wire       rst_n,
+    input  wire [7:0] duty,
+    output reg        pwm_out
+);
+
+    reg [7:0] counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            counter <= 0;
+        else
+            counter <= counter + 1;
+    end
+
+    always @(*) begin
+        pwm_out = (counter < duty);
+    end
+
+endmodule
