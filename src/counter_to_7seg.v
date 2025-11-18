3b61da9c01b012af5059211ae8495ba89b201f6a
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-18 11:39:12 +0530

    Update counter_to_7seg.v

--- src/counter_to_7seg.v
+++ src/counter_to_7seg.v
@@ -1,22 +1,15 @@
-module counter_to_7seg(
+module counter_to_7seg (
     input  wire [3:0] val,
     output reg  [6:0] seg
 );
-
     always @(*) begin
+        // default all segments off
+        seg = 7'b111_1111; // depends on common anode/cathode; choose a defined default
         case (val)
             4'h0: seg = 7'b1000000;
             4'h1: seg = 7'b1111001;
-            4'h2: seg = 7'b0100100;
-            4'h3: seg = 7'b0110000;
-            4'h4: seg = 7'b0011001;
-            4'h5: seg = 7'b0010010;
-            4'h6: seg = 7'b0000010;
-            4'h7: seg = 7'b1111000;
-            4'h8: seg = 7'b0000000;
-            4'h9: seg = 7'b0010000;
+            // ... all cases ...
             default: seg = 7'b1111111;
         endcase
     end
-
 endmodule
