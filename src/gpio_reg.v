a0543865e8960c98d8241018d4730bcbd169752b
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-18 10:37:54 +0530

    Create gpio_reg.v

---
+++ src/gpio_reg.v
@@ -0,0 +1,19 @@
+module gpio_reg(
+    input  wire       clk,
+    input  wire       rst_n,
+    input  wire       we,
+    input  wire [7:0] wdata,
+    output wire [7:0] rdata,
+    output reg  [7:0] gpio_out
+);
+
+    assign rdata = gpio_out;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            gpio_out <= 8'h00;
+        else if (we)
+            gpio_out <= wdata;
+    end
+
+endmodule
